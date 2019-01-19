c9af81ee30393e3662d806cce300133cc0d51612
Gone <actionscript@outlook.com>
2019-01-19 17:48:49 +0700

    add cocoaPod

---
+++ testProjectAppysphere/testProjectAppysphere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProjectAppysphere
+//
+//  Created by UdrinkIDev on 19/1/2562 BE.
+//  Copyright © 2562 ANUPONG DORKKUM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
